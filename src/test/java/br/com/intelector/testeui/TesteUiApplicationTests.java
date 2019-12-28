134f3cf55a926dea626d0311ea93687f48672269
thiago <thiago.alessandro.farias@gmail.com>
2019-12-28 01:03:02 -0300

    Teste com selenium

---
+++ src/test/java/br/com/intelector/testeui/TesteUiApplicationTests.java
@@ -0,0 +1,14 @@
+package br.com.intelector.testeui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteUiApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("teste");
+    }
+
+}
